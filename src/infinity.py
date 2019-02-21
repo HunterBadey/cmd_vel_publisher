344556b96582223c3ed626c922221a236ff2f5b1
Hunter <hbadey@trincoll.edu>
2019-02-21 20:52:39 +0000

    commit2

--- src/infinity.py
+++ src/infinity.py
@@ -10,14 +10,14 @@ pub = rospy.Publisher ('/cmd_vel', Twist)
 rate = rospy.Rate(100)
 velocity = Twist()
 velocity.linear.x = 1
-velocity.angular.z = 0.5
+velocity.angular.z = (math.pi / 4)
 time = 0
 
 while not rospy.is_shutdown(): 
   pub.publish(velocity)
   time = time + 0.01
   rate.sleep()
-  if time > 4 * math.pi:
+  if time >= (8):
     velocity.angular.z = velocity.angular.z * (-1)
     time = 0
   
---
+++ src/infinity_2.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import rospy
+import math
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+rospy.init_node('infinity_publisher2')
+pub = rospy.Publisher ('/cmd_vel', Twist)
+
+
+orientation = Odometry()
+orientation.position.xold = 0
+orientation.position.xnew = 0
+orientation.position.xtotal = 0
+
+def distance_calculator:
+  orientation.position.xnew = orientation.position.xold + (1* .01)
+  orientation.position.xtotal = (orientation.position.xnew - orientation.position.xold) + orientation.position.xtotal
+  orientation.position.xold = orientation.position.xnew
+
+rate = rospy.Rate(100)
+velocity = Twist()
+velocity.linear.x = 1
+velocity.angular.z = 0.5
+
+
+
+while not rospy.is_shutdown(): 
+  pub.publish(velocity)
+  sub = rospy.Subscriber ('/odom', Odometry, distance_calculator)
+  rate.sleep()
+  if orientation.position.xtotal >= (4 * math.pi):
+    velocity.angular.z = velocity.angular.z * (-1)
+    
+  
